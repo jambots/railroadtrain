2790ec1e729040e1405b572bf41ccee165763e80
Terrance Schubring <tschubring@gmail.com>
2017-01-11 18:46:38 -0600

    darken tanks, android res

--- source/upload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$data = substr($_POST['imageData'], strpos($_POST['imageData'], ",") + 1);
-$decodedData = base64_decode($data);
-$fp = fopen("trans/".$_POST['fileName'].".png", 'wb');
-fwrite($fp, $decodedData);
-fclose();
-
-?>
--- upload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$data = substr($_POST['imageData'], strpos($_POST['imageData'], ",") + 1);
-$decodedData = base64_decode($data);
-$fp = fopen("sprites/".$_POST['folder']."/".$_POST['fileName'].".png", 'wb');
-fwrite($fp, $decodedData);
-fclose();
-
-?>
